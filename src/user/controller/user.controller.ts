c69bf85867595ab7ab3f142e6c7ca38be24c8050
raineer <delaritaraineer81@gmail.com>
2022-10-29 21:54:15 +0800

    docker yaml

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserI } from '../models/user.interface';
 import { UserService } from '../service/user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
+
+  @Post()
+  add(@Body() user: UserI): Observable<UserI> {
+    return this.userService.add(user);
+  }
+
+  @Get()
+  findAll(): Observable<UserI[]> {
+    return this.userService.findAll();
+  }
 }
